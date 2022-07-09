6c2a216098c600487d5d8e3dbd0c6a224c330d3d
Humberto de Paiva Moreira <humbertomoreira93@gmail.com>
2022-07-09 13:07:58 -0300

    feat: not found movie message

--- src/layouts/MoviesGallery/index.tsx
+++ src/layouts/MoviesGallery/index.tsx
@@ -3,6 +3,7 @@ import MovieCard from "../../components/MovieCard";
 import Wraper from "../../components/Wraper";
 import { useMoviesDB } from "../../hooks/MoviesDB";
 import styles from "./styles.module.scss";
+import { TbMovieOff } from "react-icons/tb";
 
 type Movie = {
 	title: string;
@@ -16,6 +17,7 @@ const MoviesGallery = () => {
 	const { popularMovies, selectedGenres } = useMoviesDB();
 	const [filteredMovies, setFilteredMovies] =
 		useState<Movie[]>(popularMovies);
+	const [emptySearch, setEmptySearch] = useState(false);
 
 	useEffect(() => {
 		console.log(selectedGenres);
@@ -44,24 +46,42 @@ const MoviesGallery = () => {
 				return movie;
 		});
 
-		if (movies.length > 0) setFilteredMovies(movies);
-		else setFilteredMovies(popularMovies);
+		if (movies.length > 0) {
+			setFilteredMovies(movies);
+			setEmptySearch(false);
+		} else setFilteredMovies(popularMovies);
+
+		if (movies.length === 0 && selectedGenres.length > 0)
+			setEmptySearch(true);
 	}, [selectedGenres, popularMovies]);
 
 	return (
 		<main className={styles.content}>
 			<Wraper>
 				<ul>
-					{filteredMovies.map((movie) => (
-						<li key={movie.id}>
-							<MovieCard
-								src={movie.poster_path}
-								releaseDate={movie.release_date}
-								title={movie.title}
-							/>
-						</li>
-					))}
+					{!emptySearch &&
+						filteredMovies.map((movie) => (
+							<li key={movie.id}>
+								<MovieCard
+									src={movie.poster_path}
+									releaseDate={movie.release_date}
+									title={movie.title}
+								/>
+							</li>
+						))}
 				</ul>
+				{emptySearch && (
+					<div className={styles.notFound}>
+						<i>
+							<TbMovieOff />
+						</i>
+						<h2>Nenhum filme encontrado</h2>
+						<p>
+							Tente escolher outros gêneros ou diminua a
+							quantidade de gêneros selecionados
+						</p>
+					</div>
+				)}
 			</Wraper>
 		</main>
 	);
